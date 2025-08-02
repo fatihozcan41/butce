1d1a4d0a8fddae29e9032ce7a84c8749e129e1bd
fatihozcan41 <fatih@etkiakademi.com>
2025-08-02 10:29:40 +0300

    Add files via upload

--- utils/dagitim.py
+++ utils/dagitim.py
@@ -1,10 +1,23 @@
 import streamlit as st
 import pandas as pd
 
+aylar = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+         "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
+
+def aylar_arasi_dagilim(tutar, baslangic, bitis):
+    ay_sayisi = bitis - baslangic + 1
+    dagilim = [0] * 12
+    if ay_sayisi <= 0:
+        return dagilim
+    pay = tutar / ay_sayisi
+    for i in range(baslangic - 1, bitis):
+        dagilim[i] = pay
+    return dagilim
+
 def dagit_verileri(df, oranlar):
-    osgb_rows = []
-    belge_rows = []
-    belge_alt_rows = []
+    osgb_rows = {}
+    belge_rows = {}
+    belge_alt_rows = {}
 
     for _, satir in df.iterrows():
         hesap = satir.get("HESAP İSMİ", "GENEL")
@@ -14,6 +27,16 @@ def dagit_verileri(df, oranlar):
             tutar = float(tutar_raw or 0)
         except:
             tutar = 0
+
+        baslangic = satir.get("Başlangıç", 1)
+        bitis = satir.get("Bitiş", 1)
+        if pd.isna(baslangic) or pd.isna(bitis):
+            continue
+        try:
+            baslangic = int(baslangic)
+            bitis = int(bitis)
+        except:
+            continue
 
         if oran.empty:
             osgb, belge = 50, 50
@@ -28,22 +51,38 @@ def dagit_verileri(df, oranlar):
 
         osgb_pay = tutar * osgb / 100
         belge_pay = tutar * belge / 100
+        osgb_aylik = aylar_arasi_dagilim(osgb_pay, baslangic, bitis)
+        belge_aylik = aylar_arasi_dagilim(belge_pay, baslangic, bitis)
+        egitim_aylik = aylar_arasi_dagilim(belge_pay * egitim / 100, baslangic, bitis)
+        ilk_aylik = aylar_arasi_dagilim(belge_pay * ilk / 100, baslangic, bitis)
+        kalite_aylik = aylar_arasi_dagilim(belge_pay * kalite / 100, baslangic, bitis)
+        uzmanlik_aylik = aylar_arasi_dagilim(belge_pay * uzmanlik / 100, baslangic, bitis)
 
-        osgb_rows.append([hesap, osgb_pay])
-        belge_rows.append([hesap, belge_pay])
-        belge_alt_rows.append([hesap, "Eğitim", belge_pay * egitim / 100])
-        belge_alt_rows.append([hesap, "İlk Yardım", belge_pay * ilk / 100])
-        belge_alt_rows.append([hesap, "Kalite", belge_pay * kalite / 100])
-        belge_alt_rows.append([hesap, "Uzmanlık", belge_pay * uzmanlik / 100])
+        osgb_rows.setdefault(hesap, [0]*12)
+        belge_rows.setdefault(hesap, [0]*12)
+        for i in range(12):
+            osgb_rows[hesap][i] += osgb_aylik[i]
+            belge_rows[hesap][i] += belge_aylik[i]
+
+        for isim, dagilim in zip(
+            ["Eğitim", "İlk Yardım", "Kalite", "Uzmanlık"],
+            [egitim_aylik, ilk_aylik, kalite_aylik, uzmanlik_aylik]
+        ):
+            belge_alt_rows.setdefault((hesap, isim), [0]*12)
+            for i in range(12):
+                belge_alt_rows[(hesap, isim)][i] += dagilim[i]
 
     st.markdown("### 🟦 OSGB Dağıtımı")
-    df_osgb = pd.DataFrame(osgb_rows, columns=["HESAP İSMİ", "TUTAR (TL)"])
-    st.dataframe(df_osgb)
+    df1 = pd.DataFrame.from_dict(osgb_rows, orient="index", columns=aylar)
+    df1.index.name = "HESAP İSMİ"
+    st.dataframe(df1)
 
     st.markdown("### 🟨 BELGE Dağıtımı")
-    df_belge = pd.DataFrame(belge_rows, columns=["HESAP İSMİ", "TUTAR (TL)"])
-    st.dataframe(df_belge)
+    df2 = pd.DataFrame.from_dict(belge_rows, orient="index", columns=aylar)
+    df2.index.name = "HESAP İSMİ"
+    st.dataframe(df2)
 
     st.markdown("### 🧩 BELGE Alt Kırılım Dağılımı")
-    df_belge_alt = pd.DataFrame(belge_alt_rows, columns=["HESAP İSMİ", "ALT KIRILIM", "TUTAR (TL)"])
-    st.dataframe(df_belge_alt)
+    df3 = pd.DataFrame.from_dict(belge_alt_rows, orient="index", columns=aylar)
+    df3.index.names = ["HESAP İSMİ", "ALT KIRILIM"]
+    st.dataframe(df3)
